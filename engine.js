031a9bff30d06276aa83c1f35f62c482e09a46a4
qubas9 <dovrtik@gmail.com>
2025-07-15 18:34:43 +0200

    npm suport

--- engine.js
+++ engine.js
@@ -6,6 +6,17 @@ import Block from './blocks/block.js';
 import MovingBlock from './blocks/movingBlock.js';
 import Entity from './entities/entity.js'; 
 
+/**
+ * @typedef {import('./render.js').default} Render
+ * @typedef {import('./sprite.js').default} Sprite
+ * @typedef {import('./physic.js').default} Physic
+ * @typedef {import('./gameloop.js').default} GameLoop
+ * @typedef {import('./blocks/block.js').default} Block
+ * @typedef {import('./blocks/movingBlock.js').default} MovingBlock
+ * @typedef {import('./entities/entity.js').default} Entity
+ */
+
+
 export {
   Entity,
   GameLoop,
